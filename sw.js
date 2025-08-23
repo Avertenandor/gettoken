462d5fa6dcd1232f3cc42b15d45e28caf79f7568
Avertenandor <youremail@example.com>
2025-08-23 23:01:21 +0300

    PWA cache refresh: bump SW to v3, add verify_list.js to CORE; bump verify_list.js version in HTML

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const VERSION='v2';
+const VERSION='v3';
 const CACHE_STATIC='static-'+VERSION;
-const CORE=['./','index.html','css/style.css','js/app.js','js/verifier.js','js/compiler.worker.js','js/utils.js','offline.html'];
+const CORE=['./','index.html','css/style.css','js/app.js','js/verifier.js','js/verify_list.js','js/compiler.worker.js','js/utils.js','offline.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_STATIC).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.filter(k=>k!==CACHE_STATIC).map(k=>caches.delete(k))); await self.clients.claim(); })());});
 async function staleWhileRevalidate(event){
